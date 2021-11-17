4c0bfaf033fea15bd1fd73bbc7c5d226370241e8
yazid-hub <56079118+yazid-hub@users.noreply.github.com>
2021-11-17 17:22:09 +0100

    main_1.2

--- projet2/src/main/java/Main_test/Main.java
+++ projet2/src/main/java/Main_test/Main.java
@@ -5,19 +5,22 @@ import Données.PersonneDAO;
 import java.sql.*;
 import java.util.List;
 import java.util.Scanner;
+import javax.swing.JFrame;
 
 public class Main {
        public static void main(String[] args) throws SQLException {
-            
-             
+          
             Scanner sc = new Scanner(System.in);
-            int choice; 
+            int choice = 0 ; 
             String order;
+            String stop ="o";
+            do{
             System.out.println("--------MENU----------");
             System.out.println("1.ajouter");
             System.out.println("2.supprimer");
             System.out.println("3.Rechercher");
             System.out.println("4.Afficher");
+            System.out.println("0.Quitter");
             
             System.out.println("Entrez un choix(1-4)");
             choice=sc.nextInt();
@@ -26,17 +29,17 @@ public class Main {
             switch(choice){
                 case 1:
                     PersonneDAO  personneDAO = new PersonneDAO();//nait@yaghooo.fr
-                    System.out.println("Entrez votre nom:"); 
-                    String nom = sc.nextLine(); 
+                     System.out.println("Entrez votre nom:"); 
+                    String nom = sc.next();
                     
                     System.out.println("Entrez votre prenom:"); // demander au client d'inserer son prenom
-                    String prenom = sc.nextLine();
+                    String prenom = sc.next();
                     
                     System.out.println("Entrez votre tel:");
-                    String tel = sc.nextLine();
+                    String tel = sc.next();
                     
                     System.out.println("Entrez votre email:");
-                    String email = sc.nextLine();
+                    String email = sc.next();
                     
                     //iserer la personne  dans la base de données 
                     Personne unePersonne = new Personne(""+nom,""+prenom,""+tel,""+email);
@@ -51,7 +54,7 @@ public class Main {
                     break;
                  case 3:
                     System.out.println("Entrez le mail de la personne que vous rechercher:");
-                    String mail = sc.nextLine();
+                    String mail = sc.next();
                     PersonneDAO.rechercherParmail(mail);
 
                     List<Personne> personnes1 = PersonneDAO.selectionner(mail);
@@ -67,9 +70,13 @@ public class Main {
                     System.out.println("personne="+ personne);   
                     });
                     break;
+                    
+                   
+                  
                 default:
                        System.out.println("il existe pas ce numéro dans le menu :");
             }
+            }while(stop !="n");
              /*
              
                //affihcer la liste des personnes
